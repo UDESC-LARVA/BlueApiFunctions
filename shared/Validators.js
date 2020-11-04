97aa764a47d2cbdf817a2b3b8ab3360f91c64218
Adam Mews <adammews.devs@gmail.com>
2020-11-04 20:57:11 -0300

    Update Validators.js

--- shared/Validators.js
+++ shared/Validators.js
@@ -127,7 +127,7 @@ exports.calibrationOverviewSaveValidator = (calibrationOverviewReq) => {
 exports.createTokenValidator = (createTokenReq) => {
 
     let rules = {
-        userId: ['required', 'regex:^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$']
+        userId: ['required', 'regex:/^[0-9a-fA-F]{24}$/i']
     };
 
     let validation = new Validator(createTokenReq, rules);
